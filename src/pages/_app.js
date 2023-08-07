86c044584dbae34e3b37179e2c5a46632240ea95
Prem Prakash Shinde <shindeprem102@gmail.com>
2023-08-07 18:37:27 +0530

    created home and T-Shirt page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Navbar from "../../components/Navbar";
+import Carousel from "../../components/Carousel";
+import Footer from "../../components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      {/* <Carousel /> */}
+
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
